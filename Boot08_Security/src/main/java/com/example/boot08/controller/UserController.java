24fdad65d32a0baf6e3a0aa93a856f45a3885cb3
kimgura <oli999@naver.com>
2024-07-08 17:36:01 +0900

    Boot08_Security 프로젝트 추가함

---
+++ Boot08_Security/src/main/java/com/example/boot08/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.boot08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/user/loginform")
+	public String loginform() {
+		// templates/user/loginform.html 페이지로 forward 이동해서 응답 
+		return "user/loginform";
+	}
+	
+	//로그인이 필요한 요청경로를 로그인 하지 않은 상태로 요청하면 리다일렉트 되는 요청경로 
+	@GetMapping("/user/required_loginform")
+	public String required_loginform() {
+		return "user/required_loginform";
+	}
+	// POST 방식 /user/login 요청후 로그인 성공인경우 forward 이동될 url 
+	@PostMapping("/user/login_success")
+	public String loginSuccess() {
+		return "user/login_success";
+	}
+	
+	//로그인 폼을 제출(post) 한 로그인 프로세즈 중에 forward 되는 경로이기 때문에 @PostMapping 임에 주의!
+	@PostMapping("/user/login_fail")
+	public String loginFail() {
+		//로그인 실패임을 알릴 페이지
+		return "user/login_fail";
+	}	
+}
+
+
+
+
+
+
+
