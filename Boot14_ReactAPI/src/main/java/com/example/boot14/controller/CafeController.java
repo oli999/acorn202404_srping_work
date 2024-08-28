da0f8fe2b484528b0abacbf39b77b29ceac792f6
kimgura <oli999@naver.com>
2024-08-28 10:45:59 +0900

    Boot14 에 Cafe 글 목록 응답 구현함

--- Boot14_ReactAPI/src/main/java/com/example/boot14/controller/CafeController.java
+++ Boot14_ReactAPI/src/main/java/com/example/boot14/controller/CafeController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,19 @@ public class CafeController {
 	
 	@Autowired private CafeService service;
 	
+	@GetMapping("/cafes")
+	public Map<String, Object> list(CafeDto dto){
+		//CafeDto 객체에는 GET 방식 파라미터로 전달되는 pageNum, condition, keyword 가 들어 있다.
+		
+		return service.getList(dto);
+	}
+	
+	
 	/*
 	 *  클라이언트가 json 문자열을 요청 파라미터로 전달 하기 때문에 
 	 *  파라미터를 추출할때는 @RequestBody 어노테이션이 필요하다 
 	 */
-	@PostMapping("/cafe")
+	@PostMapping("/cafes")
 	public Map<String, Object> insert(@RequestBody CafeDto dto){
 		
 		service.saveContent(dto);
--- Boot14_ReactAPI/src/main/java/com/example/boot14/service/CafeServiceImpl.java
+++ Boot14_ReactAPI/src/main/java/com/example/boot14/service/CafeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.boot14.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,49 @@ import com.example.boot14.repository.CafeDao;
 
 @Service
 public class CafeServiceImpl implements CafeService{
+	//한 페이지에 글을 몇개씩 표시할 것인지
+	final int PAGE_ROW_COUNT=5;
+	//하단 페이지 UI를 몇개씩 표시할 것인지
+	final int PAGE_DISPLAY_COUNT=5;
 	
 	@Autowired private CafeDao cafeDao;
 	@Autowired private CafeCommentDao cafeCommentDao;
 
 	@Override
 	public Map<String, Object> getList(CafeDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		//CafeDto 로 부터 페이지 번호를 얻어낸다 
+		int pageNum=dto.getPageNum();
+		//보여줄 페이지의 시작 ROWNUM
+		int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
+		//보여줄 페이지의 끝 ROWNUM
+		int endRowNum=pageNum*PAGE_ROW_COUNT;
+		
+		//하단 시작 페이지 번호 
+		int startPageNum = 1 + ((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+		//하단 끝 페이지 번호
+		int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
+		
+		//전체 글의 갯수
+		int totalRow=cafeDao.getCount(dto);
+		//전체 페이지의 갯수 구하기
+		int totalPageCount=(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+		//끝 페이지 번호가 이미 전체 페이지 갯수보다 크게 계산되었다면 잘못된 값이다.
+		if(endPageNum > totalPageCount){
+			endPageNum=totalPageCount; //보정해 준다. 
+		}
+		//위에서 계산된 startRowNum 과 endRowNum 을 dto 담고
+		dto.setStartRowNum(startRowNum);
+		dto.setEndRowNum(endRowNum);
+		//CafeDto 를 인자로 전달해서 글목록 얻어오기
+		List<CafeDto> list=cafeDao.getList(dto);
+		
+		//글목록과 페이징 처리에 관련된 정보를 Map 에 담아서 리턴해 준다. 
+		return Map.of("list", list,
+				"startPageNum", startPageNum,
+				"endPageNum", endPageNum,
+				"totalPageCount", totalPageCount,
+				"pageNum", pageNum,
+				"totalRow", totalRow);
 	}
 
 	@Override
